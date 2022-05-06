c61504fef03220d181fdb441944ae02717229410
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-05-05 23:39:56 -0500

    added all updates

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,37 @@
 class MoviesController < ApplicationController
+  def index
+    movies = Movie.all
+    render json: {message: movies}
+  end
+
+  def show
+    movie = Movie.find_by(id: params[:id])
+    render json: {message: movie}
+  end
+
+  def create
+    movie = Movie.create(
+      title: "Captain America", 
+      year: 2005, 
+      plot: "MCU film"
+    )
+    render json: {message: movie}
+  end
+
+  def update
+    movie = Movie.find_by(id: params[:id])
+    movie = Movie.update(
+      title: "Chicago", 
+      year: 2006, 
+      plot: "Hello"
+    )
+    movie.save
+    render json: {message: movie}
+  end
+
+  def destroy
+    movie = Movie.find_by(id: params[:id])
+    movie.delete
+    render json: {message: "Deleted the film"}
+  end
 end
