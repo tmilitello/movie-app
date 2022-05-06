4e9b22902fc0c329d680015a82ef4cd785780d7a
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-05-05 22:53:40 -0500

    adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/movies' => 'movies#index'
+  get '/movies/:id' => 'movies#show'
+  post '/movies' => 'movies#create'
+  patch '/movies/:id' => 'movies#update'
+  delete '/movies/:id' => 'movies#destroy'
 end
+
