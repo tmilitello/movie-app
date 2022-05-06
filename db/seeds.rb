a3ba7b5d6a1ae1dced6797bdb32a05674483508c
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-05-05 22:49:42 -0500

    adds controller and model for movvies/movie

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,14 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
-actor.save
+Actor.create(first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock")
 
+Actor.create(first_name: "Thomas", last_name: "Militello", known_for: "A")
+
+Actor.create(first_name: "Caroline", last_name: "Militello", known_for: "B")
+
+Actor.create(first_name: "Grace", last_name: "Militello", known_for: "C")
+
+Movie.create(title: "Star Wars III", year: 2002, plot: "Dark Side Loses")
+
+Movie.create(title: "Harry Potter", year: 2002, plot: "Sorcerer's Stone")
